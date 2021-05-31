19f49bd239c85edc16c9a936b00099b2f47037db
Vid Vranjek <vidvranjek@gmail.com>
2021-05-31 14:30:34 +0200

    Added brakes and failsafe

--- src/main.cpp
+++ src/main.cpp
@@ -5,91 +5,124 @@
 #define PIN_DIR_OUT_R PB3
 #define PIN_PWM_ANALOG_OUT PB0
 #define PIN_LED PB1
-#define DEADZONE_US 40
+#define DEADZONE_US 50
+#define PIN_BRAKE PB5
+#define PWM_MAX_PERIOD 100
 
 unsigned long RC_ts;
-unsigned long PWM_time;
+unsigned long Safety_ts;
 float speed = 0.0;
+int16_t PWM_value = 0;
+volatile unsigned long PWM_time = 0;
+volatile unsigned long PWM_rise_time = 0;
+bool PWM_complete = false;
+bool failsafe = false;
+
+void PWM_rising();
+void PWM_falling();
+void led_on();
+void led_off();
+void brake_set();
+void brake_release();
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(PIN_RC_PWM_IN, INPUT);
-  pinMode(PIN_DIR_OUT_F, OUTPUT);
-  pinMode(PIN_DIR_OUT_R, OUTPUT);
-  pinMode(PIN_PWM_ANALOG_OUT, OUTPUT);
-  pinMode(PIN_LED, OUTPUT);
+    // put your setup code here, to run once:
+    pinMode(PIN_RC_PWM_IN, INPUT);
+    pinMode(PIN_DIR_OUT_F, OUTPUT);
+    pinMode(PIN_DIR_OUT_R, OUTPUT);
+    pinMode(PIN_PWM_ANALOG_OUT, OUTPUT);
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_BRAKE, OUTPUT);
+
+    attachInterrupt(0, PWM_rising, RISING);
+}
+
+void PWM_rising() {
+    PWM_rise_time = micros();
+    attachInterrupt(0, PWM_falling, FALLING);
+
+    led_on();
+}
+
+void PWM_falling() {
+    PWM_time = micros() - PWM_rise_time;
+    attachInterrupt(0, PWM_rising, RISING);
+
+    led_off();
+    PWM_complete = true;
+}
+
+void loop() {
+    if (PWM_complete) {
+        PWM_complete = false;
+        Safety_ts = millis();
+
+        // Sanity check (PWM time)
+        if (PWM_time < 500 || PWM_time > 2500) {
+            PWM_value = 0;
+            
+            led_on();
+        }
+        // Get PWM value (-1000...1000)
+        else {
+            PWM_value = (int16_t)1500 - PWM_time;
+        }
+
+        // Deadzone
+        if (abs(PWM_value) < DEADZONE_US) {
+            speed = 0;
+            brake_set();
+        } else {
+            speed = (float)((PWM_value - DEADZONE_US) / 400.0);
+            brake_release();
+        }
+
+        // Speed limit
+        if (speed > 1.0)
+            speed = 1.0;
+        if (speed < -1.0)
+            speed = -1.0;
+    }
+
+    if (millis() - Safety_ts > PWM_MAX_PERIOD) {
+      failsafe = true;
+    }
+    else {
+      failsafe = false;
+    }
+
+    // Set outputs
+    if (!failsafe) {
+      if (speed > 0.0) {
+        digitalWrite(PIN_DIR_OUT_F, HIGH);
+        digitalWrite(PIN_DIR_OUT_R, LOW);
+      } else {
+          digitalWrite(PIN_DIR_OUT_F, LOW);
+          digitalWrite(PIN_DIR_OUT_R, HIGH);
+      }
+
+      analogWrite(PIN_PWM_ANALOG_OUT, (uint8_t)(fabs(speed) * 255));
+    }
+    else {
+      analogWrite(PIN_PWM_ANALOG_OUT, 0);
+      brake_set();
+      led_on();
+    }
+
+}
+
+void brake_set() {
+    digitalWrite(PIN_BRAKE, LOW);
+}
+
+void brake_release() {
+    digitalWrite(PIN_BRAKE, HIGH);
 }
 
 void led_on() {
-  digitalWrite(PIN_LED, HIGH);
+    digitalWrite(PIN_LED, HIGH);
 }
 
 void led_off() {
-  digitalWrite(PIN_LED, LOW);
+    digitalWrite(PIN_LED, LOW);
 }
-
-
-void loop() {
-/*
-  while (1) {
-
-    //Fading the LED
-    for(int i=0; i<255; i++){
-      analogWrite(PWM_ANALOG_OUT, i);
-      delay(20);
-    }
-    for(int i=255; i>0; i--){
-      analogWrite(PWM_ANALOG_OUT, i);
-      delay(20);
-    }
-  }
-*/
-
-
-
-  // put your main code here, to run repeatedly:
-
-  if (digitalRead(PIN_RC_PWM_IN)) {
-    led_on();
-
-    //analogWrite(PWM_ANALOG_OUT, 100);
-
-
-    RC_ts = micros();
-    while(digitalRead(PIN_RC_PWM_IN)) {}
-    
-    PWM_time = micros()-RC_ts;
-    int16_t PWM_value = (int16_t)1500-PWM_time;
-    
-    if (abs(PWM_value) < DEADZONE_US) {
-      speed = 0;
-    }
-    else {
-      speed = (float)((PWM_value-DEADZONE_US)/400.0);
-    }
-
-    if (speed > 1.0) speed = 1.0;
-    if (speed < -1.0) speed = -1.0;
-
-
-
-    led_off();
-  }
-
-
-
-  if (speed > 0.0) {
-    digitalWrite(PIN_DIR_OUT_F, HIGH);
-    digitalWrite(PIN_DIR_OUT_R, LOW);
-  }
-  else {
-    digitalWrite(PIN_DIR_OUT_F, LOW);
-    digitalWrite(PIN_DIR_OUT_R, HIGH);
-  }
-
-  analogWrite(PIN_PWM_ANALOG_OUT, (uint8_t)(fabs(speed)*255));
-
-
-  
-}
-
